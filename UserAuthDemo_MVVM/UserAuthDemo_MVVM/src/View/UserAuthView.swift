6788682c9aa33ef3a5096a691f1ed32e83ea77e0
seiko <seikoseikou.com@gmail.com>
2020-07-12 23:24:43 +0900

    [Delete/Add/Renamed] organized directories

---
+++ UserAuthDemo_MVVM/UserAuthDemo_MVVM/src/View/UserAuthView.swift
@@ -0,0 +1,30 @@
+//
+//  UserAuthView.swift
+//  UserAuthDemo_MVVM
+//
+//  Created by 石場清子 on 2020/07/12.
+//  Copyright © 2020 石場清子. All rights reserved.
+//
+
+import UIKit
+
+class UserAuthView: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
