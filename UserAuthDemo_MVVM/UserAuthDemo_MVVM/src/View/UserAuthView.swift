c87baa56cc592b91e32706f17a960415c7c436e0
seiko <seikoseikou.com@gmail.com>
2020-07-13 11:21:51 +0900

    [Modify] Created UI

--- UserAuthDemo_MVVM/UserAuthDemo_MVVM/src/View/UserAuthView.swift
+++ UserAuthDemo_MVVM/UserAuthDemo_MVVM/src/View/UserAuthView.swift
@@ -9,22 +9,13 @@
 import UIKit
 
 class UserAuthView: UIViewController {
-
+    @IBOutlet weak var idTexrField: UITextField!
+    @IBOutlet weak var pwTextField: UITextField!
+    @IBOutlet weak var validationLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
